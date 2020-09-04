0d283948c4841e68fd506ec24587cf6576d67488
Alberto Perez <bp.codes1@gmail.com>
2020-09-04 13:30:39 -0500

    Add item separator & some styles

--- src/components/pocastDetails/PodcastDetailsScreen.tsx
+++ src/components/pocastDetails/PodcastDetailsScreen.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Box } from 'react-native-design-utility';
+import { Box, Text } from 'react-native-design-utility';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { SearchStackRouteParamsList } from '../../navigators/types';
-import { Text } from 'react-native';
+import { FlatList, Image, StyleSheet } from 'react-native';
 
 type NavigationParams = RouteProp<SearchStackRouteParamsList, 'PodcastDetails'>;
 const PodcastDetailsScreen = () => {
@@ -10,10 +10,74 @@ const PodcastDetailsScreen = () => {
   return (
     <>
       <Box f={1} bg="white">
-        <Text>{data.podcastName}</Text>
+        <FlatList
+          ItemSeparatorComponent={() => (
+            <Box w="100%" px="sm" my="sm">
+              <Box style={{ height: StyleSheet.hairlineWidth }} bg="greyLighter" />
+            </Box>
+          )}
+          ListHeaderComponent={
+            <>
+              <Box dir="row" px="sm" mt="sm" mb="md">
+                {data.thumbnail && (
+                  <Box mr={12}>
+                    <Image
+                      source={{ uri: data.thumbnail }}
+                      style={s.thumbnail}
+                    />
+                  </Box>
+                )}
+                <Box f={1}>
+                  <Text size="lg" bold>
+                    {data.podcastName}
+                  </Text>
+                  <Text size="xs" color="grey">
+                    {data.artist}
+                  </Text>
+                  <Text size="xs" color="blueLight">
+                    Subscribed
+                  </Text>
+                </Box>
+              </Box>
+              <Box px="sm" mb="md">
+                <Text>Play last episode</Text>
+              </Box>
+              <Box px="sm" mb="md">
+                <Text bold size="lg">
+                  Episodes
+                </Text>
+              </Box>
+            </>
+          }
+          data={[{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }]}
+          renderItem={() => (
+            <Box px="sm">
+              <Text size="sm" color="grey">
+                Friday
+              </Text>
+              <Text bold>#400 - Title</Text>
+              <Text size="sm" color="grey" numberOfLines={2}>
+                Some descriptioon about some podcast about gary vee. Some
+                descriptioon about some podcast about gary vee
+              </Text>
+              <Text size="sm" color="grey">
+                3hrs. 13min
+              </Text>
+            </Box>
+          )}
+          keyExtractor={(item) => item.id}
+        />
       </Box>
     </>
   );
 };
+
+const s = StyleSheet.create({
+  thumbnail: {
+    height: 100,
+    width: 100,
+    borderRadius: 4,
+  },
+});
 
 export default PodcastDetailsScreen;
